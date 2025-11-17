86ab892b195e7c441b3fe07420d81ef73c72ed11
hamadabdul1499 <hamadabdul149@yahoo.com>
2025-11-17 14:00:45 +0000

    made changes to the sidebar

--- sustainwear/src/app/Components/DonorSidebar.tsx
+++ sustainwear/src/app/Components/DonorSidebar.tsx
@@ -5,36 +5,35 @@ import { FaHome, FaPlus, FaHistory, FaLeaf, FaUser } from 'react-icons/fa';
 export default function DonorSidebar() {
   return (
 
-    <div className="w-64 bg-[#FF6B35] flex flex-col h-full justify-center rounded-tr-2xl rounded-br-2xl">
-      
+    <div className="w-64 bg-orange flex flex-col h-screen justify-center rounded-tr-2xl rounded-br-2xl relative">
       <ul className="flex flex-col space-y-9 p-4">
 
-        <li className="rounded-full font-semibold border border-[#4B6378] text-[#4B6378] hover:bg-[#4B6378] hover:text-white transition-colors">
-          <a href="/" className="flex items-center justify-center w-full p-3">
+        <li className="rounded-full font-semibold border border-navy text-navy hover:bg-navy hover:text-white transition-colors">
+          <a href="/donor" className="flex items-center justify-center w-full p-3">
             <FaHome size={20} className="mr-3" /> Home
           </a>
         </li>
 
-        <li className="rounded-full font-semibold border border-[#4B6378] text-[#4B6378] hover:bg-[#4B6378] hover:text-white transition-colors">
-          <a href="/new-donation" className="flex items-center justify-center w-full p-3">
-            <FaPlus size={20} className="mr-3" /> New Donation
+        <li className="rounded-full font-semibold border border-navy text-navy hover:bg-navy hover:text-white transition-colors">
+          <a href="/donor/new-donation" className="flex items-center justify-center w-full p-3">
+            <FaPlus size={20} className="mr-3"/> New Donation
           </a>
         </li>
 
-        <li className="rounded-full font-semibold border border-[#4B6378] text-[#4B6378] hover:bg-[#4B6378] hover:text-white transition-colors">
-          <a href="/history" className="flex items-center justify-center w-full p-3">
+        <li className="rounded-full font-semibold border border-navy text-navy hover:bg-navy hover:text-white transition-colors">
+          <a href="/donor/donation-history" className="flex items-center justify-center w-full p-3">
             <FaHistory size={20} className="mr-3" /> Donation History
           </a>
         </li>
 
-        <li className="rounded-full font-semibold border border-[#4B6378] text-[#4B6378] hover:bg-[#4B6378] hover:text-white transition-colors">
-          <a href="/impact" className="flex items-center justify-center w-full p-3">
+        <li className="rounded-full font-semibold border border-navy text-navy hover:bg-navy hover:text-white transition-colors">
+          <a href="/donor/my-impact" className="flex items-center justify-center w-full p-3">
             <FaLeaf size={20} className="mr-3" /> My Impact
           </a>
         </li>
 
-        <li className="rounded-full font-semibold border border-[#4B6378] text-[#4B6378] hover:bg-[#4B6378] hover:text-white transition-colors">
-          <a href="/profile" className="flex items-center justify-center w-full p-3">
+        <li className="rounded-full font-semibold border border-navy text-navy hover:bg-navy hover:text-white transition-colors">
+          <a href="/donor/profile" className="flex items-center justify-center w-full p-3">
             <FaUser size={20} className="mr-3" /> My Profile
           </a>
         </li>
