780e9b403689a9aa454d2595d77340c55c14d036
hamadabdul1499 <hamadabdul149@yahoo.com>
2025-11-17 15:48:22 +0000

    added the sidebar to the layout

--- sustainwear/src/app/donor/page.tsx
+++ sustainwear/src/app/donor/page.tsx
@@ -4,7 +4,6 @@ import ItemsDonated from "../Components/data-cards/ItemsDonated";
 export default function page(){
     return(
         <div>
-            <h1 className="bg-blue-500">Hello World</h1>
             <ItemsDonated/>
             <button><Link href="/donor/my-impact">My Impact</Link></button>
         </div>
