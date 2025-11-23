81deac461c96c421023822024f6ea53c0aaff6ec
Omer453 <mo.omer4450@gmail.com>
2025-11-23 18:29:01 +0000

    Completed donor dashboard + sidebar links + merge conflict fixes

--- sustainwear/src/app/donor/layout.tsx
+++ sustainwear/src/app/donor/layout.tsx
@@ -7,13 +7,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen bg-[#F5F5F5] flex">
 
       {/* LEFT ORANGE SIDEBAR */}
-      <aside className="bg-[#FF6A3D] min-h-screen w-[260px] px-6 py-10 rounded-bl-[160px] flex flex-col gap-6">
-        <NavButton href="/test" active>Home</NavButton>
-        <NavButton href="#">New Donation</NavButton>
-        <NavButton href="#">Donation History</NavButton>
-        <NavButton href="#">My Impact</NavButton>
-        <NavButton href="#">My Profile</NavButton>
-      </aside>
+     <aside className="bg-[#FF6A3D] min-h-screen w-[260px] px-6 py-10 rounded-bl-[160px] flex flex-col gap-6">
+
+  <NavButton href="/donor" active={true}>Home</NavButton>
+
+  <NavButton href="/donor/new-donation">
+    New Donation
+  </NavButton>
+
+  <NavButton href="/donor/donation-history">
+    Donation History
+  </NavButton>
+
+  <NavButton href="/donor/my-impact">
+    My Impact
+  </NavButton>
+
+  <NavButton href="/donor/profile">
+    My Profile
+  </NavButton>
+
+</aside>
 
       {/* MAIN PAGE CONTENT */}
       <main className="flex-1 px-12 py-10">
