c22e5893e8a2dbeff29aa7f85b9745c1cea58257
lamekabraha <lamekabraha@gmail.com>
2025-12-06 04:30:07 +0000

    testing new-donation

--- sustainwear/src/app/donor/new-donation/page.tsx
+++ sustainwear/src/app/donor/new-donation/page.tsx
@@ -1,18 +1,27 @@
-import DonationForm from "@/app/Components/NewDonationComp/DonationForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import prisma from "../../../../lib/prisma";
+import DonationForm from "./DonationForm";
 
+export default async function NewDonationPage() {
+  const session = await getServerSession(authOptions);
 
-export default function NewDonationPage(){
+  if (!session || !session.user || session.user.role !== "Donor") {
+    redirect("/api/auth/signin");
+  }
+
+  const [categories, sizes, genders, conditions] = await Promise.all([
+    prisma.category.findMany({select: {categoryId: true, category: true}}),
+    prisma.size.findMany({select: {sizeId: true, size: true}}),
+    prisma.gender.findMany({select: {genderId: true, gender: true}}),
+    prisma.condition.findMany({select: {conditionId: true, condition: true}}),
+  ])
+
   return (
-    <div className="p-10 flex flex-col gap-y-15 h-screen">
-      <h1 className="text-4xl font-bold">New Donation</h1>
-      {/* Donation Form */}
-      <div>
-        <DonationForm/>
-      </div>
-      {/* List of Items */}
-      <div>
-
-      </div>
+    <div className="p-10 h-screen">
+      <h1 className="text-4xl font-bold">Distribution</h1>
+      <DonationForm categories={categories} sizes={sizes} genders={genders} conditions={conditions} />
     </div>
   )
-};
+}
--- sustainwear/src/app/utils/useAlert.tsx
+++ sustainwear/src/app/utils/useAlert.tsx
@@ -2,7 +2,7 @@
 import { AlertContext } from '@/app/context/AlertContext';
 import { useContext } from 'react';
 
-export const useAlert = () => {
+export const useAlert = (p0: string, p1: string) => {
     const context = useContext(AlertContext);
     if (!context) {
         throw new Error("useAlert must be used within an AlertProvider")
